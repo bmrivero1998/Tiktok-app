712f4faf31ec704f965d328baafcdf94bd12ac17
bmrivero1998 <100807524+bmrivero1998@users.noreply.github.com>
2026-01-06 04:46:09 +0000

    feat: se guarda el primer cambio para instalacion correcta de bootstrap

--- tiktok-app/src/App.tsx
+++ tiktok-app/src/App.tsx
@@ -1,11 +1,33 @@
 import { useState } from 'react'
-import './App.css'
+import Button from 'react-bootstrap/Button'; 
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 
 function App() {
+  const [texto, setTexto] = useState('Hola Mundo');
 
-  return <div>
-    Hola mundo
-  </div>
+  const manejarClick = () => {
+    setTexto('¡Hola desde React Bootstrap!');
+  };
+
+  return (
+    <Container className="d-flex justify-content-center align-items-center vh-100">
+      <Card style={{ width: '18rem' }} className="text-center shadow">
+        <Card.Body>
+          <Card.Title>
+            <h1>{texto}</h1>
+          </Card.Title>
+          <Card.Text>
+            Este es un ejemplo simple usando componentes.
+          </Card.Text>
+          <Button variant="primary" onClick={manejarClick}>
+            Púlsame
+          </Button>
+          
+        </Card.Body>
+      </Card>
+    </Container>
+  )
 }
 
 export default App
